bdef65087fa6ab37c4e0b8c1b454fa35dba53c38
ChenDaojun <3238875613@qq.com>
2022-07-10 23:59:34 +0800

    upload files

---
+++ init.el
@@ -0,0 +1,9 @@
+(add-to-list 'load-path
+	     (expand-file-name (concat user-emacs-directory "lisp")))
+
+(require 'init-startup)
+(require 'init-elpa)
+(require 'init-ui)
+(require 'init-packages)
+
+
