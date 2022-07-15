cc59751740a44eb49023f6b474ce3fb654a84663
ChenDaojun <3238875613@qq.com>
2022-07-15 16:46:51 +0800

    删除 init.el 中 (requive 'init-startup)后面的 1

--- init.el
+++ init.el
@@ -2,9 +2,10 @@
 ;; Copyright 2022 ChenDaojun
 ;; URL: https://github.com/ChenDaojun/.emacs.d
 
-(add-to-list 'load-path	     (expand-file-name (concat user-emacs-directory "lisp")))
+(add-to-list 'load-path
+	     (expand-file-name (concat user-emacs-directory "lisp")))
 
-(require 'init-startup)1
+(require 'init-startup)
 (require 'init-elpa)
 (require 'init-packages)
 (require 'init-ui)
